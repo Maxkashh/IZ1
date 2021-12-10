85837742e87e80671e95982c798955c33f3c9db6
MaxKashhh <44949487+Maxkashh@users.noreply.github.com>
2021-12-10 19:52:05 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <mysql/mysql.h>
+#include <iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+
+int qstate;
+class BestPlanDB : public MYSQL
+{
+public:
+	BestPlanDB() = default;
+	BestPlanDB(const BestPlanDB&) = delete;
+	~BestPlanDB() = default;
+
+	BestPlanDB& operator = (const BestPlanDB&) = delete;
+
+    int New_Task(int user_id,int new_task_id, const std::string& new_task);
+    int Update_Task(int user_id,int new_task_id, const std::string& new_task);
+    std::vector<std::string> get_all_tasks(int user_id);
+    std::vector<std::pair<int, std::vector<std::string>>>login(const std::string &user_login,const std::string &user_password);
+    int Insert_New_User(const std::string& user_login,const std::string& user_mail,const std::string& user_password);
+    int Insert_New_Task(int user_id,std::vector<std::string>tasks);
+    int Delete_Task(int user_id,int new_task_id);
+    void MakeDataBase();
+private:
+
+	MYSQL* MakeConnection() const;
+	bool CheckConnection(const MYSQL* connect);
+
+};
